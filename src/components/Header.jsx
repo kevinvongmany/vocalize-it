e66d1a0014d3ca4771d9693324a2ca8d10df5fcb
giacoomoo <mprayer565@hotmail.com>
2024-10-31 21:27:17 +1100

    HeaderStyling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const Header = () => {
     return (
-        <div>
-            <header>
-                <nav className="navbar">
-                    <ul>
-                        <li><strong>Group 04: Codey the AI</strong></li>
-                    </ul>
-                    <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="faq.html">FAQ</a></li>
-                        <li><a href="tts.html">Try it out!</a></li>
-                        <li><button id="switchThemeBtn" className="outline contrast">Switch Theme</button></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+        // useTailwind classes to mount the header to top of the page
+        // and center it    
+            
+        <header className="header w-full h-24 bg-gray-900 text-white  top-0  z-50 fixed flex items-center justify-between">
+            <strong>VocalizeIt</strong>
+            <NavBar/>   
+        </header>
+
     )
 }
 
