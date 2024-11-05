2fdb1dcf304f72aa303007be450cb77a84e13624
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-05 19:44:49 +1100

    Reactified some variables

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,83 +1,129 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
 const Content = () => {
-    return (
-        <div className='mt-20 px-20'>
-            <main className='border rounded-xl p-4 bg-indigo-950'>
-                <h1 className="mt-6 mb-12 text-3xl">Vocalise It</h1>
-                <section className="container">
-                    <div className="grid">
-                        <select id="API">
-                            <option defaultValue="responsiveVoice" disabled >Speech API</option>
-                            <option value="responsiveVoice">Responsive Voice</option>
-                            <option value="synthSpeech">SynthSpeech</option>
-                        </select>
-                        <select id="Voice">
-                            <option defaultValue="Male" disabled >Voice Options</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                    </div>
+  const [voiceType, setVoiceType] = useState(["Male", "Female"]);
+  const [selectedVoiceType, setSelectedVoiceType] = useState("Male");
+
+  const [volume, setVolume] = useState(10);
+  const [voiceSpeed, setVoiceSpeed] = useState(15);
+  const [pitch, setPitch] = useState(2);
 
 
-                    <div className="grid">
-                        <section>
-                            <textarea id="ttsUserInput" name="bio" placeholder="Write some text htmlFor Codey to read..." aria-label="Professional short bio"></textarea>
-                            <small>Keep it short and sweet!</small>
-                        </section>
-                    </div>
+  return (
+    <div className="md:mt-20 md:px-20">
+      <main className="border rounded-xl p-4 bg-indigo-950 max-w-[1200px] mx-2 md:mx-auto">
+        <h1 className="mt-6 mb-12 text-3xl">Vocalise It</h1>
+        <section className="container">
+          <div className="grid">
+            <label className="" htmlFor="Voice">
+              Voice Options
+            </label>
+            <select
+              id="Voice"
+              value={selectedVoiceType}
+              onChange={(e) => setSelectedVoiceType(e.target.value)}
+            >
+              {voiceType.map((voice, index) => (
+                <option key={index} value={voice}>
+                  {voice}
+                </option>
+              ))}
+            </select>
+          </div>
 
-                    <details>
-                        <summary role="button" className="contrast outline noBorder">Audio Output</summary>
-                        <div className="container-fluid centered">
-                            <div className="voice-settings">
-                                <label htmlFor="volume">Volume Control</label>
-                                <input type="range" id="volume" min="0" max="10" step="1" />
-                            </div>
+          <div className="grid">
+            <section>
+              <textarea
+                id="ttsUserInput"
+                name="bio"
+                placeholder="Write some text htmlFor Codey to read..."
+                aria-label="Professional short bio"
+              ></textarea>
+              <small>Keep it short and sweet!</small>
+            </section>
+          </div>
 
-                            <div className="voice-settings">
-                                <label htmlFor="speed">Voice Speed Control</label>
-                                <input type="range" id="speed" min="0" max="15" step="3" />
-                            </div>
+          <details>
+            <summary role="button" className="contrast outline noBorder">
+              Audio Output
+            </summary>
+            <div className="container-fluid centered">
+              <div className="">
+                <label htmlFor="volume">Volume Control</label>
+                <input
+                  type="range"
+                  id="volume"
+                  min="0"
+                  max="10"
+                  step="1"
+                  value={volume}
+                  onChange={(e) => setVolume(e.target.value)}
+                />
+              </div>
 
-                            <div className="voice-settings">
-                                <label htmlFor="pitch">Pitch Control</label>
-                                <input type="range" id="pitch" min="0" max="2" step="0.5" />
-                            </div>
-                        </div>
-                    </details>
+              <div className="">
+                <label htmlFor="speed">Voice Speed Control</label>
+                <input
+                  type="range"
+                  id="speed"
+                  min="0"
+                  max="15"
+                  step="3"
+                  value={voiceSpeed}
+                  onChange={(e) => setVoiceSpeed(e.target.value)}
+                />
+              </div>
 
-                    <details>
-                        <summary role="button" className="contrast outline noBorder">Translation Options</summary>
-                        <div className="grid">
-                            <select id="TranslateFrom">
-                                <option defaultValue="en" disabled >Translate From</option>
-                                <option value="en">English</option>
-                                <option value="fr">French</option>
-                                <option value="de">German</option>
-                                <option value="ja">Japanese</option>
-                                <option value="ar">Arabic</option>
-                            </select>
-                            <select id="TranslateTo">
-                                <option defaultValue="en" disabled >Translate To</option>
-                                <option value="en">English</option>
-                                <option value="fr">French</option>
-                                <option value="de">German</option>
-                                <option value="ja">Japanese</option>
-                                <option value="ar">Arabic</option>
-                            </select>
-                        </div>
-                        <section>
-                            <textarea id="langOutput" name="disabled" value = "Translated text will show here..." disabled>
-                            </textarea>
-                        </section>
-                    </details>
+              <div className="">
+                <label htmlFor="pitch">Pitch Control</label>
+                <input type="range" id="pitch" min="0" max="2" step="0.5" value={pitch} onChange={(e) => setPitch(e.target.value)} />
+              </div>
+            </div>
+          </details>
 
-                    <button id="ttsBtn" className="container-fluid">Read!</button>
-                </section>
-            </main>
-        </div>
-    )
-}
+          <details>
+            <summary role="button" className="contrast outline noBorder">
+              Translation Options
+            </summary>
+            <div className="grid">
+              <select id="TranslateFrom">
+                <option value="en" disabled>
+                  Translate From
+                </option>
+                <option value="en">English</option>
+                <option value="fr">French</option>
+                <option value="de">German</option>
+                <option value="ja">Japanese</option>
+                <option value="ar">Arabic</option>
+              </select>
+              <select id="TranslateTo">
+                <option value="en" disabled>
+                  Translate To
+                </option>
+                <option value="en">English</option>
+                <option value="fr">French</option>
+                <option value="de">German</option>
+                <option value="ja">Japanese</option>
+                <option value="ar">Arabic</option>
+              </select>
+            </div>
+            <section>
+              <textarea
+                id="langOutput"
+                name="disabled"
+                value="Translated text will show here..."
+                disabled
+              ></textarea>
+            </section>
+          </details>
 
-export default Content
+          <button id="ttsBtn" className="container-fluid">
+            Read!
+          </button>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Content;
