9884a17e0d24e9752222177226f1c9d7ef5cc3cc
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-07 19:17:23 +1100

    Set up browser router and created routes boilerplate for frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import GenClips from './components/GenClips'
+import SavedClips from './components/SavedClips'
+
 import './index.css'
 import App from './App.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <GenClips />
+      }, 
+      {
+        path: '/saved',
+        element: <SavedClips />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
